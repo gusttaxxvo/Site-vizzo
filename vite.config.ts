a96ff29dfd26c3ce66e240dec45e8cb557965f15
Mateus <mateushenrique47677@gmail.com>
2026-05-24 15:43:31 -0300

    render config

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,8 @@ export default defineConfig({
       "@assets": path.resolve(__dirname, './src/assets'),
       "@services": path.resolve(__dirname, './src/services')
     }
+  },
+  preview: {
+    allowedHosts: ['vizzo-site.onrender.com']
   }
 })
